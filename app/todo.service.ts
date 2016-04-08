65c1f0af19d5ac86880e04f886a4cfd2680d1340
Imen Chebbi <imench@allmas-tn.com>
2016-04-08 10:55:16 +0100

    Display todos list.

---
+++ app/todo.service.ts
@@ -0,0 +1,17 @@
+import {Injectable} from 'angular2/core';
+import {Todo} from './todo';
+
+@Injectable()
+export class TodoService {
+  getTodos() {
+    return TODOS;
+  }
+}
+
+var TODOS:Todo[] = [
+  {"id": 1, "name": "English homework", "description": "exercice 1", "done": false},
+  {"id": 2, "name": "Learn angular 2", "description": "Todo application", "done": true},
+  {"id": 3, "name": "Learn Javascript", "description": "Javascript application", "done": false},
+  {"id": 4, "name": "Learn Typescript", "description": "Typescript application", "done": false},
+  {"id": 5, "name": "Learn Node", "description": "Node application", "done": true}
+];
--- app/todos.component.ts
+++ app/todos.component.ts
@@ -1,10 +1,25 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
+import {Todo} from './todo';
+import {TodoService} from './todo.service';
 
 @Component({
   selector: 'my-todos',
-  templateUrl: 'app/todos.component.html'
+  templateUrl: 'app/todos.component.html',
+  providers: [TodoService],
+  styleUrls: ['app/todos.component.css']
 })
 
-export class TodosComponent {
+export class TodosComponent implements OnInit {
+  todos:Todo[];
 
+  constructor(private _todoService:TodoService) {
+  }
+
+  getTodos() {
+    this.todos = this._todoService.getTodos();
+  }
+
+  ngOnInit() {
+    this.getTodos();
+  }
 }
